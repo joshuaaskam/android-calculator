13fcbd0789fe43511d7a96fe781e0c1958951ebe
joshuaaskam <115502612+joshuaaskam@users.noreply.github.com>
2024-12-28 21:55:38 -0800

    Created sealed classes for events

---
+++ app/src/main/java/com/example/calculator/CalculatorAction.kt
@@ -0,0 +1,11 @@
+package com.example.calculator
+
+// Represents all events that can be passed to the ViewModel
+sealed class CalculatorAction {
+    data class Number(val number: Int): CalculatorAction()
+    object Clear: CalculatorAction()
+    object Delete: CalculatorAction()
+    object Decimal: CalculatorAction()
+    object Calculate: CalculatorAction()
+    data class Operation(val operation: CalculatorOperation): CalculatorAction()
+}
--- app/src/main/java/com/example/calculator/CalculatorButton.kt
+++ app/src/main/java/com/example/calculator/CalculatorButton.kt
@@ -1,4 +1,4 @@
-package com.example.calculator.ui
+package com.example.calculator
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
