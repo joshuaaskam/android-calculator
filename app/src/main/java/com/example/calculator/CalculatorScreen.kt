2b980ab3189f7bcb0c4184c36b27380223cf7bac
joshuaaskam <115502612+joshuaaskam@users.noreply.github.com>
2024-12-29 20:54:40 -0800

    Created preview composable of calculator screen

--- app/src/main/java/com/example/calculator/CalculatorScreen.kt
+++ app/src/main/java/com/example/calculator/CalculatorScreen.kt
@@ -1,11 +1,11 @@
 package com.example.calculator
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -17,15 +17,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.calculator.ui.theme.LightGray
 
 @Composable
-fun Calculator(
+fun CalculatorScreen(
     viewModel: CalculatorViewModel,
+    modifier: Modifier = Modifier,
     buttonSpacing: Dp = 8.dp,
-    modifier: Modifier = Modifier
 ) {
     val uiState by viewModel.calculatorState.collectAsState()
 
@@ -53,6 +55,16 @@ fun Calculator(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
         ){
+            CalculatorButton(
+                symbol = "AC",
+                modifier = Modifier
+                    .background(LightGray)
+                    .aspectRatio(2f)
+                    .weight(2f),
+
+            ) {
+
+            }
 
         }
 
@@ -84,4 +96,10 @@ fun Calculator(
 
         }
     }
+}
+
+@Preview
+@Composable
+fun CalculatorPreview() {
+    CalculatorScreen(viewModel = CalculatorViewModel())
 }
--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,15 +3,6 @@ package com.example.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculator.ui.theme.CalculatorTheme
 
@@ -22,7 +13,7 @@ class MainActivity : ComponentActivity() {
             CalculatorTheme {
                 val viewModel = CalculatorViewModel()
                 val buttonSpacing = 8.dp
-                Calculator(
+                CalculatorScreen(
                     viewModel = viewModel,
                     buttonSpacing = buttonSpacing
                 )
