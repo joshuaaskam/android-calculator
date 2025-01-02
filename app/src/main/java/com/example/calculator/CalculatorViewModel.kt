c2cf26d82b0869fb3d83eff12efe5c44df074f6e
joshuaaskam <115502612+joshuaaskam@users.noreply.github.com>
2025-01-01 19:50:05 -0800

    Calculator ViewModel onAction function

--- app/src/main/java/com/example/calculator/CalculatorViewModel.kt
+++ app/src/main/java/com/example/calculator/CalculatorViewModel.kt
@@ -8,4 +8,35 @@ import kotlinx.coroutines.flow.asStateFlow
 class CalculatorViewModel : ViewModel() {
     private val _calculatorState = MutableStateFlow(CalculatorState())
     val calculatorState: StateFlow<CalculatorState> = _calculatorState.asStateFlow()
+
+    fun onAction(action: CalculatorAction) {
+        when(action) {
+            is CalculatorAction.Calculate -> calculate()
+            is CalculatorAction.Decimal -> enterDecimal()
+            is CalculatorAction.Delete -> delete()
+            is CalculatorAction.Number -> enterNumber(action.number)
+            is CalculatorAction.Operation -> enterOperation(action.operation)
+            is CalculatorAction.Clear -> _calculatorState.value = CalculatorState()
+        }
+    }
+
+    private fun enterOperation(operation: CalculatorOperation) {
+        TODO("Not yet implemented")
+    }
+
+    private fun enterNumber(number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    private fun delete() {
+        TODO("Not yet implemented")
+    }
+
+    private fun enterDecimal() {
+        TODO("Not yet implemented")
+    }
+
+    private fun calculate() {
+        TODO("Not yet implemented")
+    }
 }
